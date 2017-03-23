820949dd5b498f85450c0bb1b8489801ada1fcf6
heverton <heverton.sarah@gmail.com>
2017-03-22 23:28:40 -0300

    Adding heroes detail route

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,6 @@ import { Component } from '@angular/core';
 			<a routerLink="/dashboard">Dashboard</a>
 			<a routerLink="/heroes">Heroes</a>
 		</nav>
-		
 		<router-outlet></router-outlet>
 	`
 })
